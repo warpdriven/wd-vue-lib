5bf2c0d40e78f01349614738b73aa22c00c4d247
zhangfujiang <18604438176@163.com>
2022-12-07 23:24:49 +0800

    Modify api configuration

--- src/main.js
+++ src/main.js
@@ -5,34 +5,31 @@ import {config} from '../src/api/api-setting'
 
 config({
     GET_PRODUCT_HTML:{
-        ajax_url:"/api/wp-admin/admin-ajax.php",
+        ajax_url:"/wp-admin/admin-ajax.php",
         action:"wd_get_woo_product_html",
         type:"POST"
     },
     GET_PRODUCTS_HTML_BY_VS:{
-        ajax_url:"/api/wp-admin/admin-ajax.php",
+        ajax_url:"/wp-admin/admin-ajax.php",
         action:"wd_get_woo_products_html_by_vs",
         type:"POST"
     },
     GET_PRODUCT_LIST_HTML:{
-        ajax_url:"/api/wp-admin/admin-ajax.php",
+        ajax_url:"/wp-admin/admin-ajax.php",
         action:"wd_get_woo_product_list_html",
         type:"POST"
     },
     GET_HANDLE_HISTORY:{
-        ajax_url:"/api/wp-admin/admin-ajax.php",
-        action:"wd_get_woo_product_handle_history",
-        type:"POST"
+        ajax_url:"/wp-admin/admin-ajax.php",
+        action:"wd_get_woo_product_handle_history"
     },
     GET_PRODUCT_CATEGORIES:{
-        ajax_url:"wp-admin/admin-ajax.php",
-        action:"wd_get_woo_product_categories",
-        type:"POST"
+        ajax_url:"/wp-admin/admin-ajax.php",
+        action:"wd_get_woo_product_categories"
     },
     GET_PRODUCTS_BY_CATEGORY:{
-        ajax_url:'/api/wp-admin/admin-ajax.php',
-        action:"wd_get_woo_products_by_category",
-        type:"POST"
+        ajax_url:'/wp-admin/admin-ajax.php',
+        action:"wd_get_woo_products_by_category"
     }
 })
 
@@ -40,3 +37,4 @@ import ElementPlus from 'element-plus';
 import 'element-plus/theme-chalk/index.css';
 
 createApp(App).use(ElementPlus).mount('#app')
+
