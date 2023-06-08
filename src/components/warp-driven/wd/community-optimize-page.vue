3cd11d46592bbcb84e2821f496b7940acf5377be
ZhangSheng <yanglee2421@gmail.com>
2023-06-08 14:15:59 +0800

    cacel btn

--- src/components/warp-driven/wd/community-optimize-page.vue
+++ src/components/warp-driven/wd/community-optimize-page.vue
@@ -1,177 +1,233 @@
 <template>
-  <div class='optimize-page'>
+  <div class="optimize-page">
     <!-- <el-tag>optimize page</el-tag> -->
     <el-divider content-position="left">Select Product Cate</el-divider>
     <el-row>
       <el-col :span="2">&nbsp;</el-col>
-      <el-col :span="8" style="height:400px;overflow:auto;">
+      <el-col :span="8" style="height: 400px; overflow: auto">
         <el-tree
-          :data='data'
+          :data="data"
           show-checkbox
           default-expand-all
-          node-key='term_id'
-          ref='tree'
+          node-key="term_id"
+          ref="tree"
           highlight-current
-          :props='defaultProps'
+          :props="defaultProps"
         ></el-tree>
       </el-col>
-      <el-col :span='14'>
-        <el-progress
-          type='circle'
-          :stroke-width='20'
-          :width='350'
-          :color='colors'
-          :percentage='product_plan_credit ===0?0:towNumber((product_credit/product_plan_credit)*100)'
-          :format='InitializeFormat'
-        ></el-progress>
-      </el-col>
-    </el-row>
-    <el-row>
-      <el-col :span="10">&nbsp;</el-col>
       <el-col :span="14">
-        <el-button type="primary" @click="startBulkOptimization" :loading="loading" :disabled="disabled">start</el-button>
+        <div>
+          <el-progress
+            type="circle"
+            :stroke-width="20"
+            :width="350"
+            :color="colors"
+            :percentage="
+              product_plan_credit === 0
+                ? 0
+                : (product_credit / product_plan_credit) * 100
+            "
+            :format="InitializeFormat"
+          ></el-progress>
+        </div>
+        <div class="right__btn-box">
+          <el-button
+            @click="startBulkOptimization"
+            :loading="loading"
+            :disabled="disabled"
+            type="primary"
+            size="large"
+            class="right__btn"
+            round
+            >start</el-button
+          >
+          <el-button
+            @click="handleCancel"
+            :disabled="disabled"
+            type="danger"
+            link
+            >cancel</el-button
+          >
+        </div>
       </el-col>
     </el-row>
-    <el-row>
-      <el-col :span="1">&nbsp;</el-col>
+
+    <el-row justify="center">
       <el-col :span="22">
-        <el-progress :text-inside="true" :color='colors' :stroke-width="24" :percentage="task_progress" status="success"></el-progress>
+        <el-progress
+          :text-inside="true"
+          :color="colors"
+          :stroke-width="24"
+          :percentage="task_progress"
+          status="success"
+        ></el-progress>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-import {getVsCreditStatus ,getProductCategories ,getProductsByCategory} from '../../../api/wd-common-api'
+import {
+  getVsCreditStatus,
+  getProductCategories,
+  getProductsByCategory,
+} from "../../../api/wd-common-api";
 export default {
   data() {
     return {
-      loading:true,
+      loading: true,
       convert: false,
       remove: false,
       init: true,
-      task_progress:0,
-      task_status:"RUNNING",
-      product_credit:0,
-      product_plan_credit:0,
-      authenticated:true,
+      task_progress: 0,
+      task_status: "RUNNING",
+      product_credit: 0,
+      product_plan_credit: 0,
+      authenticated: true,
       colors: [
-        { color: '#f56c6c', percentage: 20 },
-        { color: '#e6a23c', percentage: 40 },
-        { color: '#5cb87a', percentage: 60 },
-        { color: '#1989fa', percentage: 80 },
-        { color: '#6f7ad3', percentage: 100 }
+        { color: "#f56c6c", percentage: 20 },
+        { color: "#e6a23c", percentage: 40 },
+        { color: "#5cb87a", percentage: 60 },
+        { color: "#1989fa", percentage: 80 },
+        { color: "#6f7ad3", percentage: 100 },
       ],
       data: [],
       defaultProps: {
-        children: 'children',
-        label: (data)=>{
-          return `${data.name}(${data.count})`
+        children: "children",
+        label: (data) => {
+          return `${data.name}(${data.count})`;
         },
-        isLeaf: (data)=>{
-          return data.count === 0
-        }
-      }
-    }
+        isLeaf: (data) => {
+          return data.count === 0;
+        },
+      },
+    };
   },
-  computed:{
-    disabled(){
-      return (this.$refs['tree']&&this.$refs['tree'].getCheckedKeys().length === 0) || this.task_status === "RUNNING" || this.product_credit < 1
-    }
+  computed: {
+    disabled() {
+      return (
+        (this.$refs["tree"] &&
+          this.$refs["tree"].getCheckedKeys().length === 0) ||
+        this.task_status === "RUNNING" ||
+        this.product_credit < 1
+      );
+    },
   },
   mounted() {
     this.getVsCreditStatus();
-    this.queryCatetgoryTree()
+    this.queryCatetgoryTree();
   },
   methods: {
-    towNumber(val) {      
-      return val.toFixed(2)    
+    towNumber(val) {
+      return val.toFixed(2);
     },
     InitializeFormat(percentage) {
-      return `${percentage}%(${this.product_credit}/${this.product_plan_credit})\n Initialize visual search`
+      const value = Number(percentage) || 0;
+      const disNum = value && value.toFixed(2);
+      return `${disNum}%(${this.product_credit}/${this.product_plan_credit})\n Initialize visual search`;
     },
-    loadTaskData(taskStatus){
-        this.task_progress = taskStatus.task_progress
-        this.product_credit = taskStatus.product_credit
-        this.product_plan_credit = taskStatus.product_plan_credit
-        this.task_status=taskStatus.task_status
+    loadTaskData(taskStatus) {
+      this.task_progress = taskStatus.task_progress;
+      this.product_credit = taskStatus.product_credit;
+      this.product_plan_credit = taskStatus.product_plan_credit;
+      this.task_status = taskStatus.task_status;
     },
-    getVsCreditStatus(){
-      this.loading = true
-      getVsCreditStatus().then(res=>{
-        this.$emit("task-status",res)
-        if(res.status){
-          this.loadTaskData(res.data)
-          if(res.data.task_status==="RUNNING"){
-            setTimeout(this.getVsCreditStatus,1000)
-          }else{
-            this.loading = false
-            if(res.data.task_status||res.data.task_status==="SUCCESS"){
-              this.task_progress= 100
-            }else{
-              this.task_progress=0
+    getVsCreditStatus() {
+      this.loading = true;
+      getVsCreditStatus().then((res) => {
+        this.$emit("task-status", res);
+        if (res.status) {
+          this.loadTaskData(res.data);
+          if (res.data.task_status === "RUNNING") {
+            setTimeout(this.getVsCreditStatus, 1000);
+          } else {
+            this.loading = false;
+            if (res.data.task_status || res.data.task_status === "SUCCESS") {
+              this.task_progress = 100;
+            } else {
+              this.task_progress = 0;
             }
           }
         }
-      })
+      });
     },
-    queryCatetgoryTree(){
-      getProductCategories({}).then(res=>{
-        this.data = this.getTreeNodes(res)
-      })
+    queryCatetgoryTree() {
+      getProductCategories({}).then((res) => {
+        this.data = this.getTreeNodes(res);
+      });
     },
-    getTreeNodes(nodes){
+    getTreeNodes(nodes) {
       let rootNodes = [];
-      nodes.forEach(node=>{
-        if(node.parent == 0){
-          rootNodes.push(node)
-          this.walkCategoryTree(nodes,node)
+      nodes.forEach((node) => {
+        if (node.parent == 0) {
+          rootNodes.push(node);
+          this.walkCategoryTree(nodes, node);
         }
       });
-      return rootNodes
+      return rootNodes;
     },
-    walkCategoryTree(nodes,parentNode){
-      let childNodes = []
-      nodes.forEach(node => {
-        if(node.parent == parentNode.term_id){
-          childNodes.push(node)
-          this.walkCategoryTree(nodes,node)
-        }  
+    walkCategoryTree(nodes, parentNode) {
+      let childNodes = [];
+      nodes.forEach((node) => {
+        if (node.parent == parentNode.term_id) {
+          childNodes.push(node);
+          this.walkCategoryTree(nodes, node);
+        }
       });
-      parentNode['children'] = childNodes
+      parentNode["children"] = childNodes;
     },
-    walkGetProductCountByCategroy(nodes){
+    walkGetProductCountByCategroy(nodes) {
       let count = 0;
-      if(nodes){
-        nodes.forEach(node=>{
-          count += node.count
-        })
+      if (nodes) {
+        nodes.forEach((node) => {
+          count += node.count;
+        });
       }
-      return count
+      return count;
     },
-    startBulkOptimization(){
-      const checkedKeys = this.$refs['tree'].getCheckedKeys()
-      const checkedNodes = this.$refs['tree'].getCheckedNodes();
-      if(checkedKeys.length === 0){
-        return
+    startBulkOptimization() {
+      const checkedKeys = this.$refs["tree"].getCheckedKeys();
+      const checkedNodes = this.$refs["tree"].getCheckedNodes();
+      if (checkedKeys.length === 0) {
+        return;
       }
-      const count=this.walkGetProductCountByCategroy(checkedNodes)
+      const count = this.walkGetProductCountByCategroy(checkedNodes);
 
-      if(count>this.product_credit){
+      if (count > this.product_credit) {
         this.$message.error({
-          message: `Select up to image vector left number ${this.product_credit} messages`
-        })
-        return
+          message: `Select up to image vector left number ${this.product_credit} messages`,
+        });
+        return;
       }
-      
-      this.loading = true
-      getProductsByCategory({category:checkedKeys.join(","),per_page:100}).then(res=>{
-        this.loading = false
-      })
-      setTimeout(this.getVsCreditStatus,1000)
-    }  
-  }
-}
+
+      this.loading = true;
+      getProductsByCategory({
+        category: checkedKeys.join(","),
+        per_page: 100,
+      }).then((res) => {
+        this.loading = false;
+      });
+      setTimeout(this.getVsCreditStatus, 1000);
+    },
+    // Cancel btn click handle
+    handleCancel() {
+      const checkedKeys = this.$refs["tree"].getCheckedKeys();
+      const checkedNodes = this.$refs["tree"].getCheckedNodes();
+      if (checkedKeys.length === 0) {
+        return;
+      }
+      const count = this.walkGetProductCountByCategroy(checkedNodes);
+
+      if (count > this.product_credit) {
+        this.$message.error({
+          message: `Select up to image vector left number ${this.product_credit} messages`,
+        });
+        return;
+      }
+      console.log(checkedKeys.join(","));
+    },
+  },
+};
 </script>
 
 <style scoped>
@@ -196,5 +252,17 @@ export default {
 .optimize-page /deep/ .el-progress__text {
   white-space: break-spaces;
   font-size: 18px !important;
+}
+
+.right__btn-box {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1rem;
+
+  padding-top: 1rem;
+}
+.right__btn {
+  width: 15rem;
 }
 </style>
--- src/components/warp-driven/wd/optimize-page.vue
+++ src/components/warp-driven/wd/optimize-page.vue
@@ -1,54 +1,56 @@
 <template>
-  <div class='optimize-page'>
+  <div class="optimize-page">
     <el-tag>optimize page</el-tag>
     <el-divider content-position="left">Select Product Cate</el-divider>
-    <el-row style="height:700px;">
-      <el-col :span="8" style="height:100%;overflow:auto;">
+    <el-row style="height: 700px">
+      <el-col :span="8" style="height: 100%; overflow: auto">
         <el-tree
-          :data='data'
+          :data="data"
           show-checkbox
           default-expand-all
-          node-key='term_id'
-          ref='tree'
+          node-key="term_id"
+          ref="tree"
           highlight-current
-          :props='defaultProps'
+          :props="defaultProps"
         ></el-tree>
       </el-col>
-      <el-col :span='16'>
+      <el-col :span="16">
         <el-progress
-          type='circle'
-          :stroke-width='20'
-          :width='300'
-          :color='colors'
-          :percentage='ivpercentage'
-          :format='InitializeFormat'
+          type="circle"
+          :stroke-width="20"
+          :width="300"
+          :color="colors"
+          :percentage="ivpercentage"
+          :format="InitializeFormat"
         ></el-progress>
         <br />
-        <el-checkbox v-model='init'>Initialize visual search</el-checkbox>
+        <el-checkbox v-model="init">Initialize visual search</el-checkbox>
         <el-row>
-          <el-col :span='12'>
+          <el-col :span="12">
             <el-progress
               :type="'circle'"
-              :stroke-width='20'
+              :stroke-width="20"
               :width="'300'"
-              :color='colors'
-              :percentage='webpercentage'
-              :format='ConvertFormat'
+              :color="colors"
+              :percentage="webpercentage"
+              :format="ConvertFormat"
             ></el-progress>
             <br />
-            <el-checkbox v-model='convert'>Convert to WebP</el-checkbox>
+            <el-checkbox v-model="convert">Convert to WebP</el-checkbox>
           </el-col>
-          <el-col :span='12'>
+          <el-col :span="12">
             <el-progress
               :type="'circle'"
-              :stroke-width='20'
-              :width="'300'"
-              :color='colors'
-              :percentage='rmpercentage'
-              :format='RemoveFormat'
+              :stroke-width="20"
+              :width="300"
+              :color="colors"
+              :percentage="rmpercentage"
+              :format="RemoveFormat"
             ></el-progress>
             <br />
-            <el-checkbox v-model='remove'>Remove pictrue background</el-checkbox>
+            <el-checkbox v-model="remove"
+              >Remove pictrue background</el-checkbox
+            >
           </el-col>
         </el-row>
       </el-col>
@@ -57,152 +59,183 @@
     <el-row>
       <el-col :span="1">&nbsp;</el-col>
       <el-col :span="22">
-        <el-button type="primary" @click="startBulkOptimization" :loading="loading" :disabled="disabled">Start Bulk Optimization</el-button>
+        <el-button
+          type="primary"
+          @click="startBulkOptimization"
+          :loading="loading"
+          :disabled="disabled"
+          >Start Bulk Optimization</el-button
+        >
       </el-col>
     </el-row>
     <el-row>
       <el-col :span="1">&nbsp;</el-col>
       <el-col :span="22">
-        <el-progress v-show="task_status==='RUNNING'" :text-inside="true" :color='colors' :stroke-width="24" :percentage="task_progress" status="success"></el-progress>
+        <el-progress
+          v-show="task_status === 'RUNNING'"
+          :text-inside="true"
+          :color="colors"
+          :stroke-width="24"
+          :percentage="task_progress"
+          status="success"
+        ></el-progress>
       </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
-import {initProducts, getVsCreditStatus ,getProductCategories ,getProductsByCategory} from '../../../api/wd-common-api'
+import {
+  initProducts,
+  getVsCreditStatus,
+  getProductCategories,
+  getProductsByCategory,
+} from "../../../api/wd-common-api";
 
 export default {
   data() {
     return {
-      loading:true,
+      loading: true,
       convert: false,
       remove: false,
       init: true,
-      task_progress:0,
-      task_status:"RUNNING",
-      image_vector_left:0,
-      image_vector_plan:0,
-      webp_left:0,
-      webp_plan:0,
-      bk_rm_left:0,
-      bk_rm_plan:0,
+      task_progress: 0,
+      task_status: "RUNNING",
+      image_vector_left: 0,
+      image_vector_plan: 0,
+      webp_left: 0,
+      webp_plan: 0,
+      bk_rm_left: 0,
+      bk_rm_plan: 0,
       colors: [
-        { color: '#f56c6c', percentage: 20 },
-        { color: '#e6a23c', percentage: 40 },
-        { color: '#5cb87a', percentage: 60 },
-        { color: '#1989fa', percentage: 80 },
-        { color: '#6f7ad3', percentage: 100 }
+        { color: "#f56c6c", percentage: 20 },
+        { color: "#e6a23c", percentage: 40 },
+        { color: "#5cb87a", percentage: 60 },
+        { color: "#1989fa", percentage: 80 },
+        { color: "#6f7ad3", percentage: 100 },
       ],
       data: [],
       defaultProps: {
-        children: 'children',
-        label: (data)=>{
-          return `${data.name}(${data.count})`
+        children: "children",
+        label: (data) => {
+          return `${data.name}(${data.count})`;
         },
-        isLeaf: (data)=>{
-          return data.count === 0
-        }
-      }
-    }
+        isLeaf: (data) => {
+          return data.count === 0;
+        },
+      },
+    };
   },
-  computed:{
-    disabled(){
-      return (this.$refs['tree']&&this.$refs['tree'].getCheckedKeys().length === 0) || this.task_status === "RUNNING" || this.image_vector_left < 1
+  computed: {
+    disabled() {
+      return (
+        (this.$refs["tree"] &&
+          this.$refs["tree"].getCheckedKeys().length === 0) ||
+        this.task_status === "RUNNING" ||
+        this.image_vector_left < 1
+      );
     },
-    ivpercentage(){
-      return this.image_vector_plan ===0?0:(this.image_vector_left/this.image_vector_plan)*100;
+    ivpercentage() {
+      return this.image_vector_plan === 0
+        ? 0
+        : (this.image_vector_left / this.image_vector_plan) * 100;
     },
-    rmpercentage(){
-      console.info(this.image_vector_left)
-      return this.bk_rm_plan ===0?0:(this.bk_rm_left/this.bk_rm_plan)*100;
+    rmpercentage() {
+      console.info(this.image_vector_left);
+      return this.bk_rm_plan === 0
+        ? 0
+        : (this.bk_rm_left / this.bk_rm_plan) * 100;
     },
-    webpercentage(){
-      return this.webp_plan ===0?0:(this.webp_left/this.webp_plan)*100
-    }
+    webpercentage() {
+      return this.webp_plan === 0 ? 0 : (this.webp_left / this.webp_plan) * 100;
+    },
   },
   mounted() {
     this.getVsCreditStatus();
-    this.queryCatetgoryTree()
+    this.queryCatetgoryTree();
+    console.log(this);
   },
   methods: {
     InitializeFormat(percentage) {
-      return `${percentage}%(${this.image_vector_left}/${this.image_vector_plan})\n Initialize visual search`
+      return `${percentage}%(${this.image_vector_left}/${this.image_vector_plan})\n Initialize visual search`;
     },
     RemoveFormat(percentage) {
-      return `${percentage}%(${this.bk_rm_left}/${this.bk_rm_plan})\n Remove pictrue background`
+      return `${percentage}%(${this.bk_rm_left}/${this.bk_rm_plan})\n Remove pictrue background`;
     },
     ConvertFormat(percentage) {
-      return `${percentage}%(${this.webp_left}/${this.webp_plan})\n Convert to WebP`
+      return `${percentage}%(${this.webp_left}/${this.webp_plan})\n Convert to WebP`;
     },
-    loadTaskData(taskStaus){
-        this.task_progress = taskStaus.task_progress
-        this.image_vector_left = taskStaus.image_vector_left
-        this.image_vector_plan = taskStaus.image_vector_plan
-        this.webp_left = taskStaus.webp_left
-        this.webp_plan = taskStaus.webp_plan
-        this.bk_rm_left = taskStaus.bk_rm_left
-        this.bk_rm_plan = taskStaus.bk_rm_plan
-        this.task_status=taskStaus.task_status
+    loadTaskData(taskStaus) {
+      this.task_progress = taskStaus.task_progress;
+      this.image_vector_left = taskStaus.image_vector_left;
+      this.image_vector_plan = taskStaus.image_vector_plan;
+      this.webp_left = taskStaus.webp_left;
+      this.webp_plan = taskStaus.webp_plan;
+      this.bk_rm_left = taskStaus.bk_rm_left;
+      this.bk_rm_plan = taskStaus.bk_rm_plan;
+      this.task_status = taskStaus.task_status;
     },
-    getVsCreditStatus(){
-      this.loading = true
-      getVsCreditStatus().then(res=>{
-        this.$emit("task-status",res)
-        if(res.status){
-          this.loadTaskData(res.data)
-          if(res.data.task_status==="RUNNING"){
-            setTimeout(this.getVsCreditStatus,1000)
-          }else{
-            this.task_progress = 0
-            this.loading = false
+    getVsCreditStatus() {
+      this.loading = true;
+      getVsCreditStatus().then((res) => {
+        this.$emit("task-status", res);
+        if (res.status) {
+          this.loadTaskData(res.data);
+          if (res.data.task_status === "RUNNING") {
+            setTimeout(this.getVsCreditStatus, 1000);
+          } else {
+            this.task_progress = 0;
+            this.loading = false;
           }
         }
-      })
+      });
     },
-    queryCatetgoryTree(){
-      getProductCategories({}).then(res=>{
-        this.data = this.getTreeNodes(res)
-      })
+    queryCatetgoryTree() {
+      getProductCategories({}).then((res) => {
+        this.data = this.getTreeNodes(res);
+      });
     },
-    getTreeNodes(nodes){
+    getTreeNodes(nodes) {
       let rootNodes = [];
-      nodes.forEach(node=>{
-        if(node.parent == 0){
-          rootNodes.push(node)
-          this.walkCategoryTree(nodes,node)
+      nodes.forEach((node) => {
+        if (node.parent == 0) {
+          rootNodes.push(node);
+          this.walkCategoryTree(nodes, node);
         }
       });
-      return rootNodes
+      return rootNodes;
     },
-    walkCategoryTree(nodes,parentNode){
-      let childNodes = []
-      nodes.forEach(node => {
-        if(node.parent == parentNode.term_id){
-          childNodes.push(node)
-          this.walkCategoryTree(nodes,node)
-        }  
+    walkCategoryTree(nodes, parentNode) {
+      let childNodes = [];
+      nodes.forEach((node) => {
+        if (node.parent == parentNode.term_id) {
+          childNodes.push(node);
+          this.walkCategoryTree(nodes, node);
+        }
       });
-      parentNode['children'] = childNodes
+      parentNode["children"] = childNodes;
     },
-    startBulkOptimization(){
-      const checkedKeys = this.$refs['tree'].getCheckedKeys()
-      if(checkedKeys.length === 0){
-        return
+    startBulkOptimization() {
+      const checkedKeys = this.$refs["tree"].getCheckedKeys();
+      if (checkedKeys.length === 0) {
+        return;
       }
-      this.loading = true
-      getProductsByCategory({category:checkedKeys.join(","),per_page:100}).then(res=>{
-          initProducts({products:res}).then(result=>{
-            if(result.status){
-              this.getVsCreditStatus()
-            }else{
-              this.loading = false
-            }
-          })
-      })
-    }  
-  }
-}
+      this.loading = true;
+      getProductsByCategory({
+        category: checkedKeys.join(","),
+        per_page: 100,
+      }).then((res) => {
+        initProducts({ products: res }).then((result) => {
+          if (result.status) {
+            this.getVsCreditStatus();
+          } else {
+            this.loading = false;
+          }
+        });
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
